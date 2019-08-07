c736feaa1be891640b95ec93bbd2ca626119aa12
sushan Gagneja <sushangagneja@IN-sushangagneja.local>
2019-08-07 14:01:52 +0530

    [Sushan] Add. Adds the basic solution.

--- src/main/java/RnaTranscription.java
+++ src/main/java/RnaTranscription.java
@@ -1,7 +1,18 @@
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String rnaTranscribedStrand = "";
+        for(int i=0;i<dnaStrand.length();i++) {
+            String tmp = "";
+            switch(dnaStrand.charAt(i)) {
+                case 'G': tmp = "C"; break;
+                case 'C': tmp = "G"; break;
+                case 'T': tmp = "A"; break;
+                case 'A': tmp = "U"; break;
+            }
+            rnaTranscribedStrand = rnaTranscribedStrand + tmp;
+        }
+        return rnaTranscribedStrand;
     }
 
 }
